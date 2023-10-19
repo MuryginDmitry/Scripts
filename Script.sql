50c5ba4dd1f46ddc76c4d6693ce90b18b05fc219
DmitryM <tihiydon@list.ru>
2023-10-19 23:28:57 +0300

    Scripts updated

--- Script.sql
+++ Script.sql
@@ -1,3 +1,8 @@
-create table NETOLOGY.CLIENT(
-id varchar (255)
+create table netology.PERSONS (
+name varchar (30),
+surname varchar (30),
+age int,
+phone_number int,
+city_of_living varchar (30),
+primary key (name, surname, age)
 );
